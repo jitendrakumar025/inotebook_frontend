1ded92ffa7c3f0b7422bc242a1a54549c2b12899
Jitendra Kumar <jitendrakumarbunkar85@gmail.com>
2023-03-07 13:29:47 +0530

    navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -15,7 +15,7 @@ const handleLogout=()=>{
     <>
     <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">iNotebook</Link>
+    <Link className="navbar-brand" to="/">iNoteBook</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
