8ea894e26c24bbe4f32949cd528ff41dacc6ce90
Jitendra Kumar <jitendrakumarbunkar85@gmail.com>
2023-08-29 00:10:21 +0530

    bot added

--- src/component/Home.js
+++ src/component/Home.js
@@ -12,8 +12,9 @@ const Home = (props) => {
          {/* <Popup /> */}
         <AddNote showAlert={showAlert} />
         <Divider/>
-      <NotesForm showAlert={showAlert} />
+        <NotesForm showAlert={showAlert} />
       </Box>
+      <iframe height="430" width="350" src="https://bot.dialogflow.com/536a3108-3771-41e1-884a-8f0621ed8678"></iframe>
     </Box>
     </>
   );
